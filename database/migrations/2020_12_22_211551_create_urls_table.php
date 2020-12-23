cf405ee6f762dcdd94da8a399eee0b7ca0143264
Thomas di Luccio <thomas@zenhysteria.fr>
2020-12-23 16:10:59 +0100

    Add missing migration

---
+++ database/migrations/2020_12_22_211551_create_urls_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUrlsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('urls', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->text('triad');
+            $table->text('url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('urls');
+    }
+}
